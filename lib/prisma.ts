1555c6bb86e1e183f5586c710a3a142859bbcaf3
Billion2608 <23004110@st.vlute.edu.vn>
2026-08-15 22:26:50 +0700

    Fix: prisma client init

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 
-// Khắc phục lỗi "Do not know how to serialize a BigInt" khi truyền dữ liệu
-(BigInt.prototype as any).toJSON = function () {
-  return Number(this);
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
 };
-
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ??
