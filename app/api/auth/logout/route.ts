3ada6d5d46e98c593b0ecd86ea8da8ae151b9259
Billion2608 <23004110@st.vlute.edu.vn>
2026-08-16 01:57:27 +0700

    Update route.ts

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,14 +1,11 @@
 import { NextResponse } from "next/server";
 
 export async function POST() {
-  const response = NextResponse.json({ message: "Đã đăng xuất" });
-
-  // Xóa cookie token xác thực
+  const response = NextResponse.json({ message: "Đăng xuất thành công" });
   response.cookies.set("token", "", {
     httpOnly: true,
     expires: new Date(0),
     path: "/",
   });
-
   return response;
 }
